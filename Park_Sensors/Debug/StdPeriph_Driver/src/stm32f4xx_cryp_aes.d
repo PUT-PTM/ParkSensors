6fa11411ec2e002e461b485b3310c4052d88a3fc
rav97 <70rafi@gmail.com>
2018-04-10 15:49:39 +0200

    Github support

--- Park_Sensors/Debug/StdPeriph_Driver/src/stm32f4xx_cryp_aes.d
+++ Park_Sensors/Debug/StdPeriph_Driver/src/stm32f4xx_cryp_aes.d
@@ -1,104 +1,104 @@
 StdPeriph_Driver/src/stm32f4xx_cryp_aes.o: \
  ../StdPeriph_Driver/src/stm32f4xx_cryp_aes.c \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
- C:/workspace/Park_Sensors/CMSIS/device/stm32f4xx.h \
- C:/workspace/Park_Sensors/CMSIS/core/core_cm4.h \
- C:/workspace/Park_Sensors/CMSIS/core/core_cmInstr.h \
- C:/workspace/Park_Sensors/CMSIS/core/core_cmFunc.h \
- C:/workspace/Park_Sensors/CMSIS/core/core_cm4_simd.h \
- C:/workspace/Park_Sensors/CMSIS/device/system_stm32f4xx.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_conf.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_adc.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_can.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_crc.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dac.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dma.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_exti.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_flash.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_hash.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rng.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_spi.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_tim.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_usart.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
- C:/workspace/Park_Sensors/StdPeriph_Driver/inc/misc.h
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/device/stm32f4xx.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cm4.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cmInstr.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cmFunc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cm4_simd.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/misc.h
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
 
-C:/workspace/Park_Sensors/CMSIS/device/stm32f4xx.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/device/stm32f4xx.h:
 
-C:/workspace/Park_Sensors/CMSIS/core/core_cm4.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cm4.h:
 
-C:/workspace/Park_Sensors/CMSIS/core/core_cmInstr.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cmInstr.h:
 
-C:/workspace/Park_Sensors/CMSIS/core/core_cmFunc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cmFunc.h:
 
-C:/workspace/Park_Sensors/CMSIS/core/core_cm4_simd.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core/core_cm4_simd.h:
 
-C:/workspace/Park_Sensors/CMSIS/device/system_stm32f4xx.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/device/system_stm32f4xx.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_conf.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_adc.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_can.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_can.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_crc.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dac.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_dma.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_exti.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_flash.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_hash.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rng.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_spi.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_tim.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_usart.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
 
-C:/workspace/Park_Sensors/StdPeriph_Driver/inc/misc.h:
+C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc/misc.h:
--- Park_Sensors/Debug/Utilities/subdir.mk
+++ Park_Sensors/Debug/Utilities/subdir.mk
@@ -21,7 +21,7 @@ Utilities/%.o: ../Utilities/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F407VGTx -DSTM32F4DISCOVERY -DDEBUG -DSTM32F40XX -DSTM32F40_41xxx -DUSE_STDPERIPH_DRIVER -I"C:/workspace/Park_Sensors/Utilities" -I"C:/workspace/Park_Sensors/StdPeriph_Driver/inc" -I"C:/workspace/Park_Sensors/inc" -I"C:/workspace/Park_Sensors/CMSIS/device" -I"C:/workspace/Park_Sensors/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F407VGTx -DSTM32F4DISCOVERY -DDEBUG -DSTM32F40XX -DSTM32F40_41xxx -DUSE_STDPERIPH_DRIVER -I"C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/Utilities" -I"C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/StdPeriph_Driver/inc" -I"C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/inc" -I"C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/device" -I"C:/Users/Rafa³/Documents/GitHub/ParkSensors/Park_Sensors/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
